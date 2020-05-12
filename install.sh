982cd44e59c88c76be8ff932ca65bbca923a6385
Victor Payno <vpayno@users.noreply.github.com>
2020-05-11 22:05:32 -0700

    Update install.sh and rename viminfo to viminfo-penguin.

--- install.sh
+++ install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [[ -f ~/.vimrc ]] && [[ ! -L ~/.vimrc ]]; then
+	mv -v ~/.vimrc{,~}
+fi
+
 ln -svf "$(pwd)/vimrc" ~/.vimrc
+
+if [[ -d ~/.kite ]] && [[ ! -L ~/.kite ]]; then
+	mv -v ~/.kite{,~}
+fi
 
 ln -svf "$(pwd)/dotconfigs/kite" ~/.kite
